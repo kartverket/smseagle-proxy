9e14d5d11aec041d83c08e254d4baebd87dc6747
Martin Haram Nygård <martin.nygard@kartverket.no>
2023-10-17 10:07:15 +0200

    add slog,goreleaser,github workflow

--- pkg/smseagle/client.go
+++ pkg/smseagle/client.go
@@ -3,29 +3,28 @@ package smseagle
 import (
 	"fmt"
 	"kartverket.no/smseagle-proxy/pkg/config"
+	"log/slog"
 	"net/http"
 )
 
 func sendSMS(cfg *config.ProxyConfig, phoneNumber string, message string) error {
 	requestUrl := fmt.Sprintf("%s/http_api/send_sms?access_token=%s&to=%s&message=%s", cfg.SMS.Url, cfg.SMS.AccessToken, phoneNumber, message)
+	slog.Debug("Sending sms", "url", requestUrl)
 	res, err := http.Get(requestUrl)
 	if err != nil {
-		fmt.Printf("error making http request: %s\n", err)
+		return err
 	}
-
-	fmt.Printf("client: got response!\n")
-	fmt.Printf("client: status code: %d\n", res.StatusCode)
+	slog.Debug("sms request succesfull", "response", res)
 	return nil
 }
 
 func call(cfg *config.ProxyConfig, phoneNumber string) error {
 	requestUrl := fmt.Sprintf("%s/http_api/call_with_termination?access_token=%s&to=%s", cfg.Call.Url, cfg.Call.AccessToken, phoneNumber)
+	slog.Debug("Sending call request", "url", requestUrl)
 	res, err := http.Get(requestUrl)
 	if err != nil {
-		fmt.Printf("error making http request: %s\n", err)
+		return err
 	}
-
-	fmt.Printf("client: got response!\n")
-	fmt.Printf("client: status code: %d\n", res.StatusCode)
+	slog.Debug("Call request successful", "response", res)
 	return nil
 }
