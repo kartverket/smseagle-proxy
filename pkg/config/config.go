6c7dc64eec088e5fc7e73182a4bfbdc99d6e1081
Martin Haram Nygård <martin.nygard@kartverket.no>
2023-10-18 14:40:55 +0200

    debug config file

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,6 +3,8 @@ package config
 import (
 	"github.com/go-playground/validator/v10"
 	"github.com/spf13/viper"
+	"io/ioutil"
+	"log"
 	"log/slog"
 	"os"
 )
@@ -23,10 +25,21 @@ type SMSEagleConfig struct {
 func Read() *ProxyConfig {
 	var cfg ProxyConfig
 	home, err := os.UserHomeDir()
-	viper.AddConfigPath(home)
-	viper.SetConfigFile("config.yaml")
+	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
-	slog.Info("home dir", "home", home)
+	viper.AddConfigPath("/opt/")
+	viper.AddConfigPath(".")
+	files, err := ioutil.ReadDir(home)
+	if err != nil {
+		log.Fatal(err)
+	}
+	slog.Info("hei")
+	slog.Info("hei", "files", files)
+	for _, file := range files {
+		slog.Info("", "file", file.Name(), "dir", file.IsDir())
+	}
+	slog.Info("Looking for config", slog.String("directory", home))
+
 	err = viper.ReadInConfig()
 	if err != nil {
 		slog.Error("fatal error config file", "error", err)
