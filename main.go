867abdf0c4ef7fed2093786add091124b8ec5582
Martin Haram Nygård <martin.nygard@kartverket.no>
2023-10-25 10:43:40 +0200

    add graceful shutdown

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/subtle"
 	"errors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -11,18 +12,32 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
-var logLevel *slog.LevelVar
+var (
+	ctx         context.Context
+	stop        context.CancelFunc
+	gracePeriod = 30 * time.Second
+	logLevel    *slog.LevelVar
+	cfg         *config.ProxyConfig
+)
 
 func init() {
+	ctx, stop = signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	cfg = config.Read()
 	logLevel = &slog.LevelVar{}
 	logLevel.Set(slog.LevelInfo)
-
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		Level: logLevel,
 	}))
 	slog.SetDefault(logger)
+	if cfg.Debug {
+		logLevel.Set(slog.LevelDebug)
+		slog.Debug("Debug mode on")
+	}
 }
 
 func basicAuth(handler http.HandlerFunc, cfg *config.ProxyConfig) http.HandlerFunc {
@@ -41,27 +56,37 @@ func basicAuth(handler http.HandlerFunc, cfg *config.ProxyConfig) http.HandlerFu
 }
 
 func main() {
-	port := ":8095"
-	slog.Info("Starting smseagle-proxy", "port", port)
-	cfg := config.Read()
-	if cfg.Debug {
-		logLevel.Set(slog.LevelDebug)
-		slog.Debug("Debug mode on")
-	}
-
 	smseagle := smseagle.NewSMSEagle(cfg)
 	oncall := alerter.NewGrafanaOncall(smseagle, cfg)
 
-	http.HandleFunc("/webhook/sms", basicAuth(oncall.HandleSMS, cfg))
-	http.HandleFunc("/webhook/call", basicAuth(oncall.HandleCall, cfg))
-	http.Handle("/metrics", promhttp.Handler())
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {})
-	err := http.ListenAndServe(port, nil)
+	sm := http.NewServeMux()
 
-	if errors.Is(err, http.ErrServerClosed) {
-		slog.Info("server closed\n")
-	} else if err != nil {
-		slog.Error("error starting server:", "error", err)
+	sm.HandleFunc("/webhook/sms", basicAuth(oncall.HandleSMS, cfg))
+	sm.HandleFunc("/webhook/call", basicAuth(oncall.HandleCall, cfg))
+	sm.Handle("/metrics", promhttp.Handler())
+	sm.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {})
+
+	server := http.Server{
+		Handler: sm,
+		Addr:    cfg.Port,
+	}
+
+	defer stop()
+	go func() {
+		slog.Info("Starting smseagle-proxy", "port", cfg.Port)
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("error starting server", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	<-ctx.Done()
+	slog.Info("graceful shutdown")
+	shutdownCtx, cancel := context.WithDeadline(context.Background(), time.Now().Add(gracePeriod))
+	defer cancel()
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		slog.Error("server shutdown error", "error", err)
 		os.Exit(1)
 	}
+	slog.Info("bye")
 }
